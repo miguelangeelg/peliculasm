7d7dfb726078bc5d0eb159864bc5ef1654107c84
miguelangeelg <63526567+miguelangeelg@users.noreply.github.com>
2020-04-20 12:49:38 -0500

    Error Enrutado
    No muestra contenido pagina Error pero ya esta enrutada, su direccion es cualquier direccion erronea

--- rutas/index.js
+++ rutas/index.js
@@ -6,12 +6,29 @@ var express=require('express'),
 
 
 
+
+function Error404(request,response,next){
+let error= new Error()   
+let local={
+    titulo: 'Pagina No Encontrada 🤕',
+    tipo:'ERROR 404',
+    error:error
+}
+error.status=404
+response.render('error',local)
+next()
+}    
+
 enrutador.get('/',(request,response,next)=>{
     let local={
         titulo:"Peliculas M"
     }
     response.render('index',local)
 })    
+
+
+enrutador.use(Error404);
+
  
 
 module.exports=enrutador;
